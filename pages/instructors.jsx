6d98993f043e46088b25daaac34f37b81c8b3e26
ammar-faifi <a@ammarf.com>
2021-08-20 11:01:11 +0300

    cleaner way to load data

--- pages/instructors.jsx
+++ pages/instructors.jsx
@@ -15,12 +15,13 @@ import styles from "../styles/evaluation-page/instructors-list.module.scss";
 import { GoSettings } from "react-icons/go";
 import Image from "next/image";
 import Head from "next/head";
-import mockData from "../dummy-data/instructors-data.json";
 import { useEffect, useState, useReducer } from "react";
 import CustomPagination from "../components/Pagination";
 import { Fade } from "react-awesome-reveal";
 import { instructorsReducer } from "../state-management/instructors-state/instructorsReducer";
+import ClientOnly from "../components/ClientOnly";
 
+import { useQuery } from "@apollo/client";
 import client from "../api/apollo-client";
 import { instructorsQuery } from "../api/queries";
 
@@ -33,38 +34,30 @@ import { instructorsQuery } from "../api/queries";
  * - Create a getStaticProps to serve the first batch of instructors
  */
 
-// To supply the initial batch of data
-export const getStaticProps = async (context) => {
-  // !WARNING: This line should be replaced by an API Query
-  console.log('Inside getStaticProps');
 
-  const { data } = await client.query({
-      query: instructorsQuery
-    })
-    
-  return {
-    props: {
-      instructorsData: data.instructors.data,
-    },
-  };
-};
-
-function instructorsList({ instructorsData }) {
+function instructorsList() {
   /*
   ? New State managemnt implementation
   - The reducer in state-managment file will handle the switching of pageContent
   - Every change in the page number will trigger a dispatch for the reducer
   */
 
-  const [pageData, dispatch] = useReducer(instructorsReducer, instructorsData);
   // ------------
   const [page, setPage] = useState(1);
   const switchPage = (pageNum) => {
     setPage(pageNum, console.log("Updated list Page num:", pageNum));
   };
 
+  useEffect(() => {
+    // ? detect the change in page number
+    console.log('Inside useEffect');
+
+    // currentList = instructorMapper(page - 1);
+
+  }, [page]);
+
   const instructorMapper = (ind) =>
-    pageData.map((instructor) => {
+    data.instructors.data.map((instructor) => {
       return (
         <InstructorCard
           image={
@@ -82,18 +75,94 @@ function instructorsList({ instructorsData }) {
         />
       );
     });
+
+  const { data, loading, error } = useQuery(instructorsQuery);
+  if (loading){
+    console.log('LOADING');
+    return (
+      <>
+        <Head>
+          <title>Petroly | Rating</title>
+        </Head>
+        <Navbar page="rating" />
+        <Container className={"mt-4 " + styles.list_container}>
+          <Row style={{ justifyContent: "center" }}>
+            <Col l={8} xs={11} md={9} xl={7} style={{ width: "100% !important" }}>
+              <InputGroup className={styles["search-container"]}>
+                <Form.Control
+                  style={{ direction: "rtl" }}
+                  type="text"
+                  placeholder="أدخِل اسم المحاضِر"
+                ></Form.Control>
+                <InputGroup.Append style={{ height: 38 }}>
+                  <Button className={styles["search_btn"]}>
+                    <BiSearch size="1.5rem" />
+                  </Button>
+                </InputGroup.Append>
+  
+                <InputGroup.Append>
+                  {/*popover for filters and order*/}
+                  <DropdownButton
+                    className={styles["dept-dropdown"]}
+                    align="start"
+                    id="dropdown-menu-align-right"
+                    title={<GoSettings size="1.5rem" />}
+                  >
+                    <Dropdown.Item className={styles["dropdown-h"]} disabled>
+                      القسم الجامعي
+                    </Dropdown.Item>
+                    <Dropdown.Divider style={{ height: "1" }} />
+                    <Dropdown.Item
+                      className={styles["depts"]}
+                      as={"div"}
+                      eventKey="1"
+                    >
+                      Software Engineering
+                    </Dropdown.Item>
+                    <Dropdown.Item
+                      className={styles["depts"]}
+                      as={"div"}
+                      eventKey="1"
+                    >
+                      Civil Engineering
+                    </Dropdown.Item>
+                    <Dropdown.Item
+                      className={styles["depts"]}
+                      as={"div"}
+                      eventKey="1"
+                    >
+                      Chemical Engineering{" "}
+                    </Dropdown.Item>
+                    <Dropdown.Item
+                      className={styles["depts"]}
+                      as={"div"}
+                      eventKey="1"
+                    >
+                      Computer Science
+                    </Dropdown.Item>
+                    <Dropdown.Item
+                      className={styles["depts"]}
+                      as={"div"}
+                      eventKey="1"
+                    >
+                      Mechanical Engineering
+                    </Dropdown.Item>
+                  </DropdownButton>
+                </InputGroup.Append>
+              </InputGroup>
+            </Col>
+          </Row>
+          <center><h1>Loading Data...</h1></center>
+        </Container>
+      </>
+    );
+  }
+
+
   var currentList = instructorMapper(page - 1);
 
-  useEffect(() => {
-    //!WARNING: We need to use async functions with the API Calls
-    console.log('Inside useEffect');
-
-    dispatch({ index: page - 1 });
-    currentList = instructorMapper(page - 1);
-
-  }, [page]);
-
   return (
+    <ClientOnly>
     <>
       <Head>
         <title>Petroly | Rating</title>
@@ -181,10 +250,11 @@ function instructorsList({ instructorsData }) {
         </Row>
 
         <div className={styles["pagination-container"]}>
-          <CustomPagination pageNum={mockData.length} switchView={switchPage} />
+          <CustomPagination pageNum={3} switchView={switchPage} />
         </div>
       </Container>
     </>
+    </ClientOnly>
   );
 }
 
