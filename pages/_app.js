594064e08b93f26148ce1706d72d26ef4d6b9152
ammar-faifi <a@ammarf.com>
2021-08-20 10:05:01 +0300

    add apollo provider

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Head from 'next/head';
 import { userContext, userReducer } from "../state-management/user-state/userContext";
 import { useReducer } from "react";
+
+import { ApolloProvider } from "@apollo/client";
+import client from "../api/apollo-client";
+
 /**
  *
  * @WARNING This file exists to only apply globals assets and context for all pages
@@ -12,21 +16,23 @@ function MyApp({ Component, pageProps }) {
   const [userInfo, dispatch] = useReducer(userReducer, {logged: false});
   return (
     <>
-      <userContext.Provider
-        value={{
-          status: userInfo,
-          userDispatch: dispatch
-        }}
-      >
-        <Head>
-          <title>Petroly</title>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
-          />
-        </Head>
-        <Component {...pageProps} />
-      </userContext.Provider>
+      <ApolloProvider client={client}>
+        <userContext.Provider
+          value={{
+            status: userInfo,
+            userDispatch: dispatch
+          }}
+        >
+          <Head>
+            <title>Petroly</title>
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+            />
+          </Head>
+          <Component {...pageProps} />
+        </userContext.Provider>
+      </ApolloProvider>
     </>
   );
 }
