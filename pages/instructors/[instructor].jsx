f11fa27bdbda9134d1007ed69bfc0a8a058ff5a8
ammar-faifi <65354445+ammar-faifi@users.noreply.github.com>
2021-08-30 21:45:16 +0300

    Update [instructor].jsx

--- pages/instructors/[instructor].jsx
+++ pages/instructors/[instructor].jsx
@@ -46,7 +46,7 @@ export const getStaticPaths = async () => {
    */
   return {
     paths: ids,
-    fallback: false,
+    fallback: true,
   };
 };
 // This function will run for each path we provided
