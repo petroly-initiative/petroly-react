b785352424370dc0a5887ea0eabd4f915007e07d
ammar-faifi <a@ammarf.com>
2021-09-01 10:46:55 +0300

    add hasEvaluated

--- pages/instructors/[instructor].jsx
+++ pages/instructors/[instructor].jsx
@@ -6,6 +6,7 @@ import {
   Button,
   OverlayTrigger,
   Tooltip,
+  Spinner,
 } from "react-bootstrap";
 import Navbar from "../../components/navbar";
 import styles from "../../styles/evaluation-page/instructors-details.module.scss";
@@ -16,12 +17,17 @@ import { AiFillEdit } from "react-icons/ai";
 import Evaluation from "../../components/evaluation/Evaluation";
 import InstructorRates from "../../components/Instructros/InstructorRates";
 import EvaluationModal from "../../components/evaluation/EvaluationModal";
-import {  useEffect, useState, useContext } from "react";
+import { useEffect, useState, useContext } from "react";
 import Image from "next/image";
 import Head from "next/head";
 import { MdFolderSpecial } from "react-icons/md";
 import client from "../../api/apollo-client";
-import { getInstructorName, getInstructorDetail } from "../../api/queries";
+import { useQuery } from "@apollo/client";
+import {
+  getInstructorName,
+  getInstructorDetail,
+  hasEvaluatedQuery,
+} from "../../api/queries";
 import { Fade } from "react-awesome-reveal";
 
 export const getStaticPaths = async () => {
@@ -55,7 +61,8 @@ export const getStaticProps = async (context) => {
 
   const { data } = await client.query({
     query: getInstructorDetail,
-    variables: {id}});
+    variables: { id },
+  });
 
   return {
     props: { data: data },
@@ -67,15 +74,40 @@ export const getStaticProps = async (context) => {
 
 export default function instructorDetails({ data }) {
   const [modalVisible, setVisible] = useState(false);
+  const [msg, setMsg] = useState("");
   const userContext = useContext(UserContext);
+
+  const { data: dataHasEvaluated, loading: loadingHasEvaluated } = useQuery(
+    hasEvaluatedQuery,
+    {
+      skip: userContext.user.status !== USER.LOGGED_IN,
+      variables: { instructorId: data.instructor.id },
+    }
+  );
+
+  useEffect(() => {
+    if (userContext.user.status === USER.LOGGED_IN) {
+      if (dataHasEvaluated) {
+        if (dataHasEvaluated.hasEvaluated) {
+          setMsg("قيمتَ المُحاضِر");
+          setVisible(false);
+          // we can redirect the user to the eavaluation edit page
+        } else setMsg("قيّم المحاضر");
+      }
+    } else setMsg("الرجاء تسجيل الدخول");
+  }, [loadingHasEvaluated, userContext.user.status]);
 
   const closeModal = () => {
     setVisible(false);
   };
 
   const launchModal = () => {
-    if (userContext.user.status === USER.LOGGED_IN) setVisible(true);
-            }
+    if (
+      userContext.user.status === USER.LOGGED_IN &&
+      !dataHasEvaluated.hasEvaluated
+    )
+      setVisible(true);
+  };
 
   const colors = [
     "rgb(235, 24, 122)",
@@ -91,8 +123,8 @@ export default function instructorDetails({ data }) {
     );
   };
   // !WARNING: Change eval structure according to specified date
-  const evalMapper = () => 
-    data.instructor.evaluationSet.data.map(evaluation => 
+  const evalMapper = () =>
+    data.instructor.evaluationSet.data.map((evaluation) => (
       <Evaluation
         date={evaluation.date.split("T")[0]}
         grading={""}
@@ -103,12 +135,11 @@ export default function instructorDetails({ data }) {
           evaluation.teaching,
           evaluation.personality,
         ]}
-        comment = {evaluation.comment}
+        comment={evaluation.comment}
         term={""}
         course={evaluation.course.toUpperCase()}
       />
-    );
-  
+    ));
 
   const evalList = evalMapper();
 
@@ -218,30 +249,42 @@ export default function instructorDetails({ data }) {
             </Card>
           </Col>
         </Row>
-        <OverlayTrigger
-          placement="top"
-          delay={{ show: 350, hide: 400 }}
-          overlay={
-            userContext.user.status === USER.LOGGED_IN ? (
-              <Tooltip id="button-tooltip-2">قيّم المحاضِر</Tooltip>
-            ) : (
-              <Tooltip id="button-tooltip-2">الرجاء تسجيل الدخول</Tooltip>
-            )
-          }
-        >
-          <Button
-            id="evaluate"
-            className={styles.evalBtn}
-            onClick={launchModal
-            }
-            style={{
-              backgroundColor:
-                userContext.user.status !== USER.LOGGED_IN ? "gray" : "#00ead3",
-            }}
+        {loadingHasEvaluated ? (
+          <OverlayTrigger
+            placement="top"
+            delay={{ show: 350, hide: 400 }}
+            overlay={<Tooltip id="button-tooltip-2">نتفقد بياناتك</Tooltip>}
           >
-            <AiFillEdit size={32} />
-          </Button>
-        </OverlayTrigger>
+            <Button
+              id="evaluate"
+              className={styles.evalBtn}
+              style={{ backgroundColor: "gray" }}
+            >
+              <Spinner animation="border" role="status" />
+            </Button>
+          </OverlayTrigger>
+        ) : (
+          <OverlayTrigger
+            placement="top"
+            delay={{ show: 350, hide: 400 }}
+            overlay={<Tooltip id="button-tooltip-2">{msg}</Tooltip>}
+          >
+            <Button
+              id="evaluate"
+              className={styles.evalBtn}
+              onClick={launchModal}
+              style={{
+                backgroundColor:
+                  userContext.user.status !== USER.LOGGED_IN ||
+                  dataHasEvaluated.hasEvaluated
+                    ? "gray"
+                    : "#00ead3",
+              }}
+            >
+              <AiFillEdit size={32} />
+            </Button>
+          </OverlayTrigger>
+        )}
         <EvaluationModal
           name={data.instructor.name}
           id={data.instructor.id}
