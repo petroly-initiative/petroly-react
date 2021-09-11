a5b00c5e5e2db861a9849cc03c74a11d42cc0774
Muhab Abubaker <mohababubakir2001@gmail.com>
2021-09-11 21:48:51 +0300

    404-500 E pages, google forms for adding instructors

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,12 +1,28 @@
-import Link from 'next/link'
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../styles/error-pages/errors.module.scss";
+import { Container } from "react-bootstrap";
 
 export default function FourOhFour() {
-  return <>
-    <h1>404 - Page Not Found</h1>
-    <Link href="/">
-      <a>
-        Go back home
-      </a>
-    </Link>
-  </>
+  return (
+    <>
+      <Container className={styles["main-container"]}>
+        <div className={styles["content-div"]}>
+          <div className={styles["error-txt"]}>
+            عذرا, الصفحة المطلوبة غير تماحة حاليا
+          </div>
+
+          <Image
+            className={styles["error-img"]}
+            src={"/images/errors/404Epage.svg"}
+            width={540}
+            height={320}
+          />
+          <div className={styles["home-btn"] + " shadow"}>
+            <Link href="/home">العودة إلى الرئيسية</Link>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
 }
--- pages/500.jsx
+++ pages/500.jsx
@@ -1,3 +1,23 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../styles/error-pages/errors.module.scss";
+import { Container } from "react-bootstrap";
+
 export default function Custom500() {
-  return <h1>500 - Server-side error occurred</h1>;
+  return (
+    <Container className={styles["main-container"]}>
+      <div className={styles["content-div"]}>
+        <div className={styles["error-txt"]}>عذرا, هنالك مشكلة في خادم الاتصال</div>
+        <Image
+          className={styles["error-img"]}
+          src={"/images/errors/500Epage.svg"}
+          width={540}
+          height={320}
+        />
+        <div className={styles["home-btn"] + " shadow"}>
+          <Link href="/home">العودة إلى الرئيسية</Link>
+        </div>
+      </div>
+    </Container>
+  );
 }
