5c5085b3acc2400bc5ca4fc61042a34db2a02ec0
Muhab Abubaker <mohababubakir2001@gmail.com>
2022-08-05 20:40:33 +0300

    Ci extra fixes

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    "retries": 2,
-    baseUrl: "http://localhost:80",
+    retries: 2,
+    baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
