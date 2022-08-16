84bd8fbb836449d9bc9bb396ec70186db4203aeb
Muhab Abubaker <mohababubakir2001@gmail.com>
2022-08-16 17:56:31 +0300

    adding google font css tags for mobile fonts compatibility

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,16 @@ class MyDocument extends Document {
       <>
         <Html lang="ar">
           <Head>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link
+              rel="preconnect"
+              href="https://fonts.gstatic.com"
+              crossorigin
+            />
+            <link
+              href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;500;600;700;800;900&display=swap"
+              rel="stylesheet"
+            ></link>
             <meta
               name="description"
               content="Digital Platform for All KFUPMers"
