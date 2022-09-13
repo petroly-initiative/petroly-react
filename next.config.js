c1f21cea52000da39638b837f957b0ca72ffbde3
Muhab Abubaker <mohababubakir2001@gmail.com>
2022-09-13 21:19:06 +0300

    containerizing the frontend

--- next.config.js
+++ next.config.js
@@ -1,17 +1,23 @@
-
 const withPWA = require("next-pwa");
 
 const nextConfig = withPWA({
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
   pwa: {
     dest: "public",
     register: true,
     skipWaiting: true,
-    disable: process.env.NODE_ENV === "development"
-
+    disable: process.env.NODE_ENV === "development",
   },
   images: {
     domains: ["res.cloudinary.com"],
   },
+  output: "standalone",
 });
-  
+
 module.exports = nextConfig;
