057287202e5ee87da0309ef8150cbaa647ad7aec
ammar-faifi <me@ammarf.sa>
2025-05-28 20:01:25 +0300

    bump versions to lates

--- next.config.js
+++ next.config.js
@@ -1,23 +1,24 @@
-const withPWA = require("next-pwa");
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
 
-const nextConfig = withPWA({
-  webpackDevMiddleware: (config) => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300,
-    };
+const nextConfig = {
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 300,
+      };
+    }
     return config;
-  },
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development",
   },
   images: {
     domains: ["res.cloudinary.com"],
   },
   output: "standalone",
-});
+};
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
